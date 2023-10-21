fc5e397017524448ac917678556037ec54efd40e
Chris Ian Fiel <ccfiel@gmail.com>
2023-10-21 10:45:48 +0800

    lten

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,6 @@
 import { CollectionConfig } from 'payload/types'
+import { isAdmin, isAdminFieldLevel } from '../access/isAdmin';
+import { isAdminOrSelf } from '../access/isAdminOrSelf';
 
 const Users: CollectionConfig = {
   slug: 'users',
@@ -6,6 +8,16 @@ const Users: CollectionConfig = {
   admin: {
     listSearchableFields: ['name', 'email'],
     useAsTitle: 'name',
+  },
+  access: {
+    // Only admins can create users
+    create: isAdmin,
+    // Admins can read all, but any other logged in user can only read themselves
+    read: isAdminOrSelf,
+    // Admins can update all, but any other logged in user can only update themselves
+    update: isAdminOrSelf,
+    // Only admins can delete
+    delete: isAdmin,
   },
   fields: [
     {
@@ -17,6 +29,29 @@ const Users: CollectionConfig = {
       name: 'profilePicture',
       type: 'upload',
       relationTo: 'media',
+    },
+    {
+      name: 'roles',
+      // Save this field to JWT so we can use from `req.user`
+      saveToJWT: true,
+      type: 'select',
+      hasMany: true,
+      defaultValue: ['user'],
+      access: {
+        // Only admins can create or update a value for this field
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+      },
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'User',
+          value: 'user',
+        },
+      ]
     },
   ],
 }
